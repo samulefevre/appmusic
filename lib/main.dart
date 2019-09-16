f4b376fb2a25ffef5c29bf8d434d390dc4610614
samuel.lefevr@gmail.com <samu@mini-de-samuel.home>
2019-09-16 02:25:01 +0200

    fixs

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'music.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {  
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -30,8 +30,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List<Music> listMusic = [
-    new Music('Theme Swift', 'Samu', 'assets/un.jpg', 'assets/un.mp3');
-    new Music('Theme Flutter', 'Samu', 'assets/deux.jpg', 'assets/deux.mp3');
+    new Music('Theme Swift', 'Samu', 'assets/un.jpg', 'assets/un.mp3'),
+    new Music('Theme Flutter', 'Samu', 'assets/deux.jpg', 'assets/deux.mp3')
   ];
 
   Music currentMusic;
@@ -40,7 +40,6 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     super.initState();
     currentMusic = listMusic[0];
-
   }
 
   AudioPlayer audioPlayer;
@@ -56,15 +55,14 @@ class _MyHomePageState extends State<MyHomePage> {
       backgroundColor: Colors.grey[800],
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
             Card(
               elevation: 9.0,
               child: Container(
-                width: MediaQuery.of(context).size.height / 2.5),
-                child : Image.asset(name),
-              
-
+                width: MediaQuery.of(context).size.height / 2.5,
+                child: Image.asset(currentMusic.imagePath),
+              ),
             )
           ],
         ),
